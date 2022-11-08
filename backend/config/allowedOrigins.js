71dbe0b818b91de1e9f5e67fe928923b63781f6d
Jedzas <kolozinskas.marius@gmail.com>
2022-11-08 19:55:09 +0200

    preparing for deployment2

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     // 'http://localhost:3000',
     // 'http://localhost:4000',
-    'https://studentassist-api.onrender.com'
+    'https://studentazzist-api.onrender.com'
 ]
 
 module.exports = allowedOrigins
--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { setCredentials } from '../../features/auth/authSlice'
 
 const baseQuery = fetchBaseQuery({
-    baseUrl: 'https://studentassist-api.onrender.com',
+    baseUrl: 'https://studentazzist-api.onrender.com',
     credentials: 'include',
     prepareHeaders: (headers, { getState }) => {
         const token = getState().auth.token
